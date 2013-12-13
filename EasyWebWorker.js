f994a532e8600c35613cb94fc31978a4cb40f1a6
Cezary <cezary.nowak@gmail.com>
2013-12-14 00:50:36 +0100

    run function added, close function renamed, readme updated

--- EasyWebWorker.js
+++ EasyWebWorker.js
@@ -11,10 +11,11 @@ var EasyWorker = (function(root) {
     if(!options) {
       options = {};
     }
+
     if(typeof data !== 'function') {
       throw new Error('EasyWorker data parameter must contain function.');
     }
-    /* XXX: Don't use inline comments in head_scripts block */
+
     var head_scripts = function() {
       /* Make console available in Worker */
       self.console = (function (c) {
@@ -25,22 +26,29 @@ var EasyWorker = (function(root) {
               self.postMessage(JSON.stringify({
                 'easyWorkerMessageType': 'console',
                 'easyWorkerMessageMethod': method,
-                'easyWorkerMessageData': Array.slice(arguments)
+                'easyWorkerMessageData': [].slice.call(arguments)
               }));
             };
           })(method);
         }
         return console;
-      })('assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn'.split(','));
-
-      self.done = function() {
+      })('debug,error,exception,group,groupCollapsed,groupEnd,info,log,time,timeEnd,warn'.split(','));
+      /* done: call it when calculations are done, pass result to your code */
+      self.done = function(/* args... */) {
         self.postMessage(JSON.stringify({
           'easyWorkerMessageType': 'done',
-          'easyWorkerMessageData': Array.slice(arguments)
+          'easyWorkerMessageData': [].slice.call(arguments)
         }));
       };
 
-      var messageCallbacks = {};
+      var messageCallbacks = {
+        /* build in message callback to support easyWorkerInstance.run() */
+        __run: function() {
+          console.info('run', arguments);
+          self.__WorkerFunction.apply(self, arguments);
+        }
+      };
+
       self.onMsg = function(type, func) {
         if(arguments.length < 2) {
           func = type;
@@ -48,29 +56,29 @@ var EasyWorker = (function(root) {
         }
         messageCallbacks[type] = func;
       };
+
       self.onmessage = function(msg) {
         var data;
         try {
           data = JSON.parse(msg.data);
         } catch (err) {
-          console.error('wrong data passed in msg', msg.data);
-          return;
+          throw new Error('wrong data passed in msg: ' + msg.data);
         }
-        return typeof messageCallbacks[data.type] === 'function' ? messageCallbacks[data.type].call(this, data.data) : null;
+        return typeof messageCallbacks[data.type] === 'function' ? messageCallbacks[data.type].apply(this, data.data) : null;
       };
     };
-    data = 'data:text/javascript;charset=UTF-8,' + encodeURIComponent('(' + head_scripts + ')();self.WorkerFunction = ' + data + ';');
+    data = 'data:text/javascript;charset=UTF-8,' + encodeURIComponent('(' + head_scripts + ')();self.__WorkerFunction=' + data + ';');
     
     if(options.autoStart) {
       if(!(options.argumentsOnAutoStart instanceof Array)) {
         throw new Error('argumentsOnAutoStart option need to be Array');
       }
-      data += encodeURIComponent('self.WorkerFunction.apply(self, '+ (options.argumentsOnAutoStart ? JSON.stringify(options.argumentsOnAutoStart) : '[]') +');');
+      data += encodeURIComponent('self.__WorkerFunction.apply(self, '+ (options.argumentsOnAutoStart ? JSON.stringify(options.argumentsOnAutoStart) : '[]') +');');
     }
     this.rawWorker = new root.Worker(data);
     var _this = this;
     this.rawWorker.addEventListener("message", function(e) {
-      var isObject = false, data;
+      var isObject, data;
       try {
         data = JSON.parse(e.data);
         isObject = true;
@@ -98,6 +106,9 @@ var EasyWorker = (function(root) {
   
   WorkerCreator.prototype = {
     msg: function(type, data) {
+      if(this.terminated) {
+        throw new Error('Worker has finished his job. You can\'t post message now');
+      }
       if(arguments.length < 2) {
         data = type;
         type = 'default';
@@ -113,38 +124,25 @@ var EasyWorker = (function(root) {
       return this;
     },
     onErrorFunc: function() {
-      console.log("WORKER ERROR: ", arguments);
+      console.error("WORKER ERROR: ", arguments);
     },
     onError: function(func) {
       this.onErrorFunc = func;
       return this;
     },
-    close: function() {
+    terminate: function() {
       this.rawWorker.terminate();
+      this.terminated = true;
       return this;
-    }
+    },
+    run: function() {
+      this.msg('__run', [].slice.call(arguments));
+      return this;
+    },
+
+    terminated: false
   };
   return function(script, options) {
     return new WorkerCreator(script, options);
   };
 })(window);
-
-var test = EasyWorker(function(count) {
-  /* do some calculations */
-  var result = [];
-  for(var i = count; i--;) {
-    result.push('element' + i);
-  }
-  onMsg('default', function(msg) {
-    postMessage('thanks for msg! It\'s your message: ' + JSON.stringify(msg));
-  });
-  console.log('uff almost ready...');
-  done(result);
-}, {
-  autoStart: true,
-  argumentsOnAutoStart: [10]
-}).onDone(function(responseResult) {
-  console.log('Yay, I\'ve received some data from worker:', responseResult);
-});
-
-test.msg('default', {"pretty message": "pretty message content"});
--- README.md
+++ README.md
@@ -1,7 +1,40 @@
-EasyWebWorkers
+Easy Web Worker
 ==============
 
-This library make use WebWorkers easier for developer
+This library make use Web Workers easier for developer
+
+
+Documentation
+==============
+
+easyWorkerInstance = EasyWorker(workerFunction[, options])
+---------------------
+EasyWorker returns new instance of EasyWorker;
+Pass workerFunction, that you want to be executed inside worker.
+options argument is plain object, and can take following fields:
+- `autoStart`: no extra message will be sent to run the worker
+- `argumentsOnAutoStart`: array of arguments passed to workerFunction on autoStart
+
+easyWorkerInstance.onDone(callback)
+---------------------
+onDone will execute when done() function is called in worker scope. It takes one
+parameter with worker result. You can terminate your worker now.
+
+easyWorkerInstance.onError(callback)
+---------------------
+onError will execute when any error will be throwed from worker.
+
+easyWorkerInstance.msg(callbackOrType, callback)
+---------------------
+Post message to the Worker. You can easly capture events by worker.onMsg
+
+easyWorkerInstance.terminate()
+---------------------
+Immediately shut down the worker. You can't reuse it, when terminate is called.
+
+easyWorkerInstance.run(args...)
+---------------------
+Run workerFunction with passed arguments.
 
 
 Sample Usage
@@ -9,26 +42,23 @@ Sample Usage
 
 ```javascript
 // EastWorker creates new instance by default, no need to add new keyword
-var test = EasyWorker(function(count) {
+var test = EasyWorker(function(prefix, count) {
   /* do some calculations */
   var result = [];
   for(var i = count; i--;) {
-    result.push('element' + i);
+    result.push(prefix + i);
   }
   onMsg('default', function(msg) {
     postMessage('thanks for msg! It\'s your message: ' + JSON.stringify(msg));
   });
   console.log('uff almost ready...');
   done(result);
-}, {
-  autoStart: true,
-  argumentsOnAutoStart: [10]
 }).onDone(function(responseResult) {
   console.log('Yay, I\'ve received some data from worker:', responseResult);
-});
+}).run('foo-', 10);
 ```
 
-Custom functions available in Worker scope
+Functions available in Worker scope
 ==============
 
 self.onMsg(callbackOrType[, callback])
@@ -36,18 +66,14 @@ self.onMsg(callbackOrType[, callback])
 Worker is expecting message with specific Type.
 Default type is 'default'
 To execute onMsg callback, defined as:
-`self.onMsg('custom', function(){ /* do something */ });`
+`self.onMsg('custom', function() { /* do something */ });`
 trigger:
 `easyWorkerInstance.msg('custom', {thisIsMy:'data'});`
 
 self.console(args...)
 ---------------------
 EasyWorker adds console functionality to Workers. You can use any of following:
-- console.assert,
-- console.count,
 - console.debug,
-- console.dir,
-- console.dirxml,
 - console.error,
 - console.exception,
 - console.group,
@@ -55,17 +81,13 @@ EasyWorker adds console functionality to Workers. You can use any of following:
 - console.groupEnd,
 - console.info,
 - console.log,
-- console.markTimeline,
-- console.profile,
-- console.profileEnd,
 - console.time,
 - console.timeEnd,
-- console.trace,
 - console.warn
 
 self.done(result)
 ---------------------
 When job is done, trigger done() function.
-Handle result by `easyworkerInstance.onDone(callback)`
+Handle result by `easyWorkerInstance.onDone(callback)`
 
 ...
